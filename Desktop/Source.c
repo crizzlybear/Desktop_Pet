ab9acd0cb6fb436a17ce87ea7d6df8da5baff3ba
Christine <christine-bui@hotmail.com>
2024-08-28 12:22:17 +0800

    Timer window movement

--- Desktop/Source.c
+++ Desktop/Source.c
@@ -1,37 +1,52 @@
 #include <windows.h>
-
-
+#define TIMER_ID 1
+int xPos = 50;
+int yPos = 50;
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+   
+    //int y = 0;
     switch (msg) {
+        case WM_CREATE: {
+            SetTimer(hwnd, TIMER_ID, 2000, NULL);
+            break;
+        }
         case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0)); // Red color
-            RECT rect = {50, 50, 200, 150};
+            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
+            RECT rect = { 50, 50, 200, 150 };
             FillRect(hdc, &rect, hBrush);
             DeleteObject(hBrush);
             EndPaint(hwnd, &ps);
-            /*Sleep(2000);
-            DestroyWindow(hwnd);*/ //auto delete
-          
             break;
         }
         case WM_LBUTTONDOWN: {
             // Close the window on left mouse button click
             MessageBoxA(NULL,"clicked","T", MB_OK);
+            
             DestroyWindow(hwnd);
            
             break;
         }
+        case WM_TIMER: {
+            //MessageBoxA(NULL, "RightClick", "T", MB_OK);
+            if (wParam == TIMER_ID) {
+                xPos = xPos + 10;
+                SetWindowPos(hwnd, HWND_TOP, xPos, 50, 200, 150, SWP_NOZORDER | SWP_NOSIZE);
+            }
+            
+            break;
+        }
         case WM_DESTROY:
-            PostQuitMessage(0);
+            KillTimer(hwnd, TIMER_ID);
+            PostQuitMessage(0);//deletes Msg
             break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
-
+//SetWindowPos(hwnd, HWND_TOP, x, 50, 200, 150, SWP_NOZORDER | SWP_NOSIZE);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
