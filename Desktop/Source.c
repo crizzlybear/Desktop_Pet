6ed72ca52b1e1a1dac8d2085ac48323fc2b39acf
Christine <christine-bui@hotmail.com>
2024-09-01 22:35:19 +0800

    randomize actions

--- Desktop/Source.c
+++ Desktop/Source.c
@@ -1,9 +1,13 @@
 #include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int xPos = 50;
 int yPos = 50;
-boolean move = TRUE;
+boolean move = FALSE;
 #define TIMER_ID 1
+#define TIMER_ID2 2
+
 HDC hMemDC;         // Memory device context
 HBITMAP hBitmap1;   // First bitmap
 /*HBITMAP hBitmap2;*/   // Second bitmap
@@ -11,12 +15,14 @@ HBITMAP hBitmap1;   // First bitmap
 HBITMAP hOldBitmap; // Old bitmap in memory DC
 int xx = 0;
 int yy = 0;
-
+int walk = 10;
+int r=0;
 boolean drag = FALSE;
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     int desktopWidth = GetSystemMetrics(SM_CXSCREEN);
-
-    int rand = 0;
+    
+    srand(time(0));
     switch (uMsg) {
     case WM_CREATE:
     {
@@ -25,7 +31,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
         // Load and select bitmaps
         
-        hBitmap1 = (HBITMAP)LoadImage(NULL,L"\\\\?\\C:\\Users\\chris\\OneDrive\\Pictures\\dogmap.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);// the \\?\ is used to bypass path limit of olderapis
+        hBitmap1 = (HBITMAP)LoadImage(NULL,L"\\\\?\\C:\\Users\\chris\\OneDrive\\Pictures\\dogmap2.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);// the \\?\ is used to bypass path limit of olderapis
         //hBitmap2 = (HBITMAP)LoadImage(NULL, L"\\\\?\\C:\\Users\\chris\\OneDrive\\Pictures\\dog_profileBMP2.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);// the \\?\ is used to bypass path limit of olderapis
 
         // Select the first bitmap into the memory DC
@@ -34,6 +40,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         ReleaseDC(hwnd, hdc2); // Release the window DC
 
         SetTimer(hwnd, TIMER_ID, 500, NULL); // Set up a timer
+        SetTimer(hwnd, TIMER_ID2, 6000, NULL); // Set up a timer
         break;
     }
     case WM_PAINT: // Triggered when the window needs to be repainted. This happens when the window is first shown, when it is resized, or when InvalidateRect is called.
@@ -48,43 +55,53 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         
         break;
     }
-    case WM_TIMER:
+    case WM_TIMER:{
         if (wParam == TIMER_ID) {
-            if (xPos < desktopWidth-100 &&move==TRUE) {
-                xPos = xPos + 10;
+            if (xPos < desktopWidth-100 && move==TRUE) {
+                xPos = xPos + walk;
             }
             
             SetWindowPos(hwnd, HWND_TOP, xPos, yPos, 100, 100, SWP_NOZORDER | SWP_NOSIZE);
-           /* ClientToScreen(hwnd, &rect);
-            MoveWindow(hwnd, xPos, yPos, 100, 100, TRUE);*/
-            // Toggle between bitmaps
-           
             if (xx == 0) {
-               xx=xx+100;
+                xx = xx + 100;
             }
             else {
                 xx = 0;
             }
+        }
+        if (wParam == TIMER_ID2) {
+            r = rand() % (4+1);
             
-            if (yPos % 2 ==0) {
+            switch (r) {
+            case 1:
                 yy = 100;
                 move = TRUE;
-            }
-            else if (yPos % 3 == 0) {
+                walk = 10;
+                break;
+            case 2:
                 yy = 200;
                 move = FALSE;
-                
-            }
-            else {
+                break;
+            case 3:
+                yy = 300;
+                move = TRUE;
+                walk = -10;
+                break;
+            default:
                 yy = 0;
                 move = FALSE;
-             
+                break;
             }
+
+        }
+
+        
+           
             
             // Trigger a repaint
             InvalidateRect(hwnd, NULL, TRUE);
             break;
-        }
+    }
     case WM_RBUTTONDOWN: {
         // Close the window on left mouse button click
         MessageBoxA(NULL,"clicked","T", MB_OK);
@@ -133,6 +150,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         DeleteDC(hMemDC); // Delete the memory DC
         DeleteObject(hBitmap1);
       /*  DeleteObject(hBitmap2);*/
+        KillTimer(hwnd, TIMER_ID);
+        KillTimer(hwnd, TIMER_ID2);
         PostQuitMessage(0);
         break;
 
