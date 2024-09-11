00ff4042875c2008628aa555f5cb62cbfb2bdac5
Christine <christine-bui@hotmail.com>
2024-09-11 23:32:37 +0800

    cleanup and comments

--- Desktop/Source.c
+++ Desktop/Source.c
@@ -1,121 +1,114 @@
 #include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-int xPos = 50;
-int yPos = 50;
-boolean move = FALSE;
+
 #define TIMER_ID 1
 #define TIMER_ID2 2
 
 HDC hMemDC;         // Memory device context
-HBITMAP hBitmap1;   // First bitmap
-/*HBITMAP hBitmap2;*/   // Second bitmap
-/*HBITMAP hCurrentBitmap;*/ // Currently selected bitmap
+HBITMAP hBitmap1;   // bitmap that holds sprite sheet
 HBITMAP hOldBitmap; // Old bitmap in memory DC
-int xx = 0;
-int yy = 0;
-int walk = 10;
-int r=0;
-boolean drag = FALSE;
+
+int xPos = 50; //window position x coord
+int yPos = 50; //window position y coord
+
+int xx = 0; //sprite x coord
+int yy = 0; //sprite y coord
+
+int walk = 10; //walk rate
+int r=0;//action selection number
+boolean drag = FALSE; //for click and drag
 int neg = -1;
-int n = 0;
-int nArray[3] = {-1,0,1};
+int n = 0;//array index
+int nArray[3] = {-1,0,1};//walk direction: down, horizontal, up
+boolean move = FALSE;
 boolean bump = FALSE;
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     int desktopWidth = GetSystemMetrics(SM_CXSCREEN);
     int desktopHeight = GetSystemMetrics(SM_CYSCREEN);
-    srand((unsigned int)time(NULL));
+    srand((unsigned int)time(NULL));//seed random
     switch (uMsg) {
-    case WM_CREATE:
+    case WM_CREATE: 
     {
+        //When window is created
         HDC hdc2 = GetDC(hwnd); // Get the window device context
         hMemDC = CreateCompatibleDC(hdc2); // Create a compatible memory DC
+        // Load and select bitmaps 
+        hBitmap1 = (HBITMAP)LoadImage(NULL,L"\\\\?\\C:\\Users\\chris\\OneDrive\\Pictures\\dogmap6.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);// the \\?\ is used to bypass path limit of olderapis
 
-        // Load and select bitmaps
-        
-        hBitmap1 = (HBITMAP)LoadImage(NULL,L"\\\\?\\C:\\Users\\chris\\OneDrive\\Pictures\\dogmap3.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);// the \\?\ is used to bypass path limit of olderapis
-        //hBitmap2 = (HBITMAP)LoadImage(NULL, L"\\\\?\\C:\\Users\\chris\\OneDrive\\Pictures\\dog_profileBMP2.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);// the \\?\ is used to bypass path limit of olderapis
-
-        // Select the first bitmap into the memory DC
+        // Select bitmap into the memory DC
         hOldBitmap = SelectObject(hMemDC, hBitmap1);
-
         ReleaseDC(hwnd, hdc2); // Release the window DC
 
-        //SetTimer(hwnd, TIMER_ID, 500, NULL); // Set up a timer
-        //SetTimer(hwnd, TIMER_ID2, 3000, NULL); // Set up a timer
-
-        SetTimer(hwnd, TIMER_ID, 300, NULL); // Set up a timer
-        SetTimer(hwnd, TIMER_ID2, 1000, NULL); // Set up a timer
+        //Create timers
+        SetTimer(hwnd, TIMER_ID, 300, NULL); // Framerate for animation
+        SetTimer(hwnd, TIMER_ID2, 1000, NULL); //Timer for changing actions
         break;
     }
-    case WM_PAINT: // Triggered when the window needs to be repainted. This happens when the window is first shown, when it is resized, or when InvalidateRect is called.
+    case WM_PAINT: 
     {
+        // WM_PAINT: Triggered when the window needs to be repainted. 
+        // ie. when the window is first shown, when it is resized, or when InvalidateRect is called.
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps); //BeginPaint: Prepares the window for painting and retrieves a device context (HDC).
   
-        BitBlt(hdc, 0, 0, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, hMemDC, xx, yy, SRCCOPY);
-   
-        
-        EndPaint(hwnd, &ps);
-        
+        BitBlt(hdc, 0, 0, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, hMemDC, xx, yy, SRCCOPY);//Draw to window from MDC
+        //ps.rcPaint: Window dimensions
+        //xx,yy are the set coordinates on the spritemap
+        EndPaint(hwnd, &ps);     
         break;
     }
     case WM_TIMER:{
-        if (wParam == TIMER_ID) {
-
-            if (move) {
-                
+        //When timer is triggered
+        if (wParam == TIMER_ID) {//on framerate 
+            //Check if action is moving then check boundaries then walk
+            if (move) { 
                 if (xPos >= desktopWidth - 100) {
-                    //MessageBoxA(NULL,"out", "Right", MB_OK);
+                    //Right boundary
                     bump = TRUE;
                     yy = 300;//walk left
                     walk = -10;
                     neg = neg * (-1);
                 }
                 else if (xPos <= 0) {
+                    //Left boundary
                     bump = TRUE;
                     neg = neg * (-1);
-                    //MessageBoxA(NULL, (char*)neg, "Left", MB_OK);
                     yy = 100;//walk right
                     walk = 10;
-
                 }
                 else if (yPos <= 0) {
-                    //MessageBoxA(NULL, "out", "Top", MB_OK);
+                    //Top boundary
                     bump = TRUE;
                     yy = 100;//walk right
                     walk = 10;
-                    neg = 1;
-                    
+                    neg = 1; 
                 }
                 else if (yPos >= desktopHeight -150) {
-                    //MessageBoxA(NULL, "out", "Bot", MB_OK);
+                    //Bottom boundary
                     bump = TRUE;
                     yy = 100;
                     walk = 10;
                     neg = -1;
-                  
-
                 }
-                
+                //set movement
                 xPos = xPos + walk;
-                //yPos = yPos + ((walk / 2) * neg );
                 yPos = yPos + ((walk / 2) * neg);
             }
-            
-
-            SetWindowPos(hwnd, HWND_TOP, xPos, yPos, 100, 100, SWP_NOZORDER | SWP_NOSIZE);
+            SetWindowPos(hwnd, HWND_TOP, xPos, yPos, 100, 100, SWP_NOZORDER | SWP_NOSIZE);//update window movement
+            //Animate by alternating between frame x=0 and x=100 from sprite sheet
             if (xx == 0) {
-                xx = xx + 100;
+                xx = 100;
             }
             else {
                 xx = 0;
             }
         }
-        if (wParam == TIMER_ID2) {
+        if (wParam == TIMER_ID2) {//for action changes
+            //Randomly select direction from array
             n = rand() % (2 + 1);//0,1,2
             neg = nArray[n];
+            //Randomly select number for action
             if ((r ==2) || (r==1)) {
                 r = rand() % (3-1+1) +1; //1,2,3  
             }
@@ -162,43 +155,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
             default:
                 yy = 0;//standing
                 move = FALSE;
-                bump = FALSE;
-               
+                bump = FALSE;               
                 break;
             }
 
         }
-
-        
-           
-            
             // Trigger a repaint
             InvalidateRect(hwnd, NULL, TRUE);
             break;
     }
     case WM_RBUTTONDOWN: {
-        // Close the window on left mouse button click
-        MessageBoxA(NULL,"clicked","T", MB_OK);
-                    
+        // Close the window on right mouse button click
+        MessageBoxA(NULL,"Closing Desktop Pet ...","Goodbye", MB_OK);             
         DestroyWindow(hwnd);
-        /*GetWindowRect(hwnd, &rect);
-        char buffer[256];
-        snprintf(buffer, sizeof(buffer), "Position:\nLeft: %ld\nTop: %ld\nRight: %ld\nBottom: %ld",
-            rect.left, rect.top, rect.right, rect.bottom);
-        MessageBoxA(hwnd, buffer, "Window Position", MB_OK | MB_ICONINFORMATION);*/
-                   
         break;
     }
     case WM_LBUTTONDOWN:
         drag = TRUE;
-        SetCapture(hwnd);
+        SetCapture(hwnd); //captures mouse input
         break;
     case WM_LBUTTONUP:
         ReleaseCapture();
         drag = FALSE;
         break;
     case WM_MOUSEMOVE:
-        //https://codingmisadventures.wordpress.com/2009/03/06/dragging-or-moving-a-window-using-mouse-win32/
+        //SOURCE: https://codingmisadventures.wordpress.com/2009/03/06/dragging-or-moving-a-window-using-mouse-win32/
         if (drag)
         {
             RECT mainWindowRect;
@@ -223,7 +204,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         SelectObject(hMemDC, hOldBitmap); // Restore the old bitmap
         DeleteDC(hMemDC); // Delete the memory DC
         DeleteObject(hBitmap1);
-      /*  DeleteObject(hBitmap2);*/
         KillTimer(hwnd, TIMER_ID);
         KillTimer(hwnd, TIMER_ID2);
         PostQuitMessage(0);
@@ -235,14 +215,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     return 0;
 }
  
- 
 
-
-
-
- 
-//SetWindowPos(hwnd, HWND_TOP, x, 50, 200, 150, SWP_NOZORDER | SWP_NOSIZE);
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+    //Create window class
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
@@ -254,26 +229,24 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
        L"SimpleTransparentClass",
        L"Transparent Window",
        WS_POPUP, //for a bordered window use: WS_OVERLAPPEDWINDOW,
-       50, 50, 100, 100,
+       50, 50, 100, 100, //xPos,yPos,width,height
        NULL,
        NULL,
        hInstance,
        NULL
    );
-  
-    //SetLayeredWindowAttributes(hwnd, 0, 200, LWA_ALPHA); // Semi-transparent
-    SetLayeredWindowAttributes(hwnd, RGB(255,0,255), 255, LWA_COLORKEY);
+    //Apply transparency
+    SetLayeredWindowAttributes(hwnd, RGB(255,0,255), 255, LWA_COLORKEY);//Set magenta to be transparent
  
-
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
-
+    
+    //Message loop to pass on to window procedure
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
  
-
     return (int)msg.wParam;
 }
